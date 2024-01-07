7c59b98d79c2015d07663f3a510173d3e41f5293
Mario-Fernando24 <3115408069.mafe@gmail.com>
2024-01-06 22:11:48 -0500

    logica cambio de pantalla

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'warena',
+      title: 'cultura app',
       home: OnbordingView(),
       initialRoute: onbordingViewRoutes,
       routes: appRoutes,
--- lib/presentation/views/onbording/onbording.dart
+++ lib/presentation/views/onbording/onbording.dart
@@ -1,3 +1,5 @@
+import 'package:culturappco/config/themes/app_style.dart';
+import 'package:culturappco/config/themes/size_config.dart';
 import 'package:culturappco/domain/models/onbording.dart';
 import 'package:flutter/material.dart';
 
@@ -9,18 +11,84 @@ class OnbordingView extends StatefulWidget {
 }
 
 class _OnbordingViewState extends State<OnbordingView> {
+
+  int currentPage = 0;
+  PageController _pageController = PageController(initialPage: 0); 
+
+  Container doIndicator(index){
+    return Container(
+      height: 10,
+      width: 10,
+      decoration: BoxDecoration(
+        color: currentPage==index? kPrimaryColor:kSecondaryColor,
+        shape: BoxShape.circle
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    SizeConfig().init(context);
+
+    double sizeH = SizeConfig.blockSizeH!;
+    double sizeV = SizeConfig.blockSizeV!;
+
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
         child: Column(
           children: [
-            Expanded(child: PageView.builder(
-              itemCount: onboardingContents.length,
-              itemBuilder: (context, index)=>
-              Text(onboardingContents[index].title)
-          ))
+            Expanded(
+              flex: 9,
+                child: PageView.builder(
+                  controller: _pageController,
+                    itemCount: onboardingContents.length,
+                    onPageChanged: (value){
+                      setState(() {
+                        currentPage =value;
+                      });
+                    },
+                    itemBuilder: (context, index) => Column(
+                          children: [
+                            SizedBox(height: sizeV * 5),
+                            Text(onboardingContents[index].title,
+                                style: kTitle, textAlign: TextAlign.center),
+                            SizedBox(height: sizeV * 5),
+                            Container(
+                                height: sizeV * 50,
+                                child: Image.asset(
+                                  onboardingContents[index].image,
+                                  fit: BoxFit.contain,
+                                )),
+                            SizedBox(height: sizeV * 5),
+                            RichText(
+                                textAlign: TextAlign.center,
+                                text: TextSpan(children: [
+                                  TextSpan(
+                                      text: "WE CAN",
+                                      style: TextStyle(color: kPrimaryColor)),
+                                  TextSpan(
+                                      text: "HELP YOU",
+                                      style: TextStyle(color: kPrimaryColor)),
+                                  TextSpan(
+                                      text: "TO BE A BETTER",
+                                      style: TextStyle(color: kPrimaryColor)),
+                                  TextSpan(
+                                      text: "VERSION OF",
+                                      style: TextStyle(color: kPrimaryColor)),
+                                  TextSpan(
+                                      text: "YOURSELF",
+                                      style: TextStyle(color: kPrimaryColor))
+                                ])),
+                            SizedBox(height: sizeV * 5),
+                          ],
+                      )
+                    )
+                  ),
+                  Expanded(flex: 1, child: Row(
+                    children: List.generate(onboardingContents.length, 
+                    (index) => doIndicator(index)),
+                  ))
           ],
         ),
       ),
